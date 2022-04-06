e83efe159ede7de618fc70b201aafddfbc3f1590
Amal-Igor <amal.bnmhd@gmail.com>
2022-04-06 13:12:21 +0200

    implementation registration

--- frontend/src/components/Forms/Forms.js
+++ frontend/src/components/Forms/Forms.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import './Forms.css'
 
 const StyledLabel = styled.label`
 font-size: 1.6rem;
@@ -11,13 +12,30 @@ padding-top: .4rem;
 padding-bottom: .4rem;
 `
 
-export const StyledInputs = ( { action ,labelClassname, labelfor, label ,name, placeholder} ) => {
+export const StyledInputs = ( { event ,labelClassname, labelfor, label ,name, placeholder} ) => {
 
 
     return (
         <>
         <StyledLabel className={labelClassname} htmlFor={labelfor}>{label}</StyledLabel>
-        <StyledInput onChange={action} type="text"  name={name} placeholder={placeholder}></StyledInput>
+        <StyledInput onChange={event} type="text"  name={name} placeholder={placeholder}></StyledInput>
+        </>
+    )
+}
+
+
+const FormWrapper = styled.form`
+display: flex;
+flex-direction: column;
+padding: 7rem 10rem;
+background-color: #0f032796;
+gap: 1.3rem;   
+`
+export const StyledForm = (props) => {
+
+    return(
+        <>
+        <FormWrapper onSubmit={props.event} className="form-wrapper">{props.children}</FormWrapper>
         </>
     )
 }
--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -1,49 +1,33 @@
-import { StyledInputs } from "../../components/Forms/Forms";
+import { StyledInputs, StyledForm } from "../../components/Forms/Forms";
 import { ButtonWoIcon } from "../../components/Button/Button";
 import { useState, useEffect } from "react";
 import { SignupService } from "../../components/Services/ServiceLayer.js"
-import './Signup.css'
 import styled from "styled-components";
 
 
-const SignupWrapper = styled.form`
-display: flex;
-flex-direction: column;
-padding: 7rem 10rem;
-background-color: #0f032796;
-gap: 1.3rem;
-`
 
-const SubmitButton = styled(ButtonWoIcon)`
-`
+
+
 const SignupForm = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-
-
-const [username, setUsername] = useState('');
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-
-
-
-
-const handleSignup = async () => {
-    const signupData = {username : `${username}`, email: `${email}`, password: `${password}`}
-    console.log(signupData);
-    await SignupService(signupData)
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
-
-}
-
-    
+    const handleSignup = async (e) => {
+        e.preventDefault();
+        const signupData = {username : `${username}`, email: `${email}`, password: `${password}`}
+        console.log(signupData);
+        await SignupService(signupData)
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
+    }
     return (
-        <form id="signup-form">
-            <StyledInputs action={(e) => {setUsername(e.target.value)}} labelClassname={'signupform-label'} labelfor={'username'} label={'Entrez un nom d\'utilsateur'} name={'username'} placeholder={'username'} />
-            <StyledInputs action={(e) => setEmail(e.target.value)} labelClassname={'signupform-label'} labelfor={'email'} label={'Entrez un email'} name={'email'} placeholder={'email'} />
-            <StyledInputs action={(e) => setPassword(e.target.value)} labelClassname={'signupform-label'} labelfor={'password'} label={'Entrez un mot de passe'} name={'password'} placeholder={'password'} />
-            <SubmitButton action={handleSignup()} text={'Créer un compte'} />
-        </form>
+        <StyledForm event={handleSignup} id="signup-form">
+            <StyledInputs event={(e) => {setUsername(e.target.value)}} labelClassname={'signupform-label'} labelfor={'username'} label={'Entrez un nom d\'utilsateur'} name={'username'} placeholder={'username'} />
+            <StyledInputs event={(e) => setEmail(e.target.value)} labelClassname={'signupform-label'} labelfor={'email'} label={'Entrez un email'} name={'email'} placeholder={'email'} />
+            <StyledInputs event={(e) => setPassword(e.target.value)} labelClassname={'signupform-label'} labelfor={'password'} label={'Entrez un mot de passe'} name={'password'} placeholder={'password'} />
+            <ButtonWoIcon text={'Créer un compte'} />
+        </StyledForm>
     )
 }
 
